09465262d69df5908237181078e94e67395e2a39
yuntaol <yuntao.lu@student.unimelb.edu.au>
2022-01-14 19:39:03 +1100

    add backend repository

---
+++ backend/diningTogether/src/test/java/com/summerHack/diningTogether/DiningTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summerHack.diningTogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend/src/main/java/com/summerHack/diningTogether/DiningTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.summerHack.diningTogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiningTogetherApplication.class, args);
+	}
+
+}
