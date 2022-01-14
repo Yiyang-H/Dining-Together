7e7bd621829e9cc274fa5a63696cd01078cbe3ea
yuntaol <yuntao.lu@student.unimelb.edu.au>
2022-01-14 21:31:11 +1100

    Add swagger

--- backend/src/main/java/com/summerHack/diningTogether/DiningTogetherApplication.java
+++ backend/src/main/java/com/summerHack/diningTogether/DiningTogetherApplication.java
@@ -1,9 +1,12 @@
 package com.summerHack.diningTogether;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Dining Together API", version = "1.0"))
 public class DiningTogetherApplication {
 
 	public static void main(String[] args) {
