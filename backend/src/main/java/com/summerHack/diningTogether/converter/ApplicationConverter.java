011d6016776fed7d62cb8ed77536f5a491dc20f9
Liu233w <gh@liu233w.com>
2022-01-17 17:54:37 +1100

    Refactor: rename Converter to converter

--- backend/src/main/java/com/summerHack/diningTogether/converter/ApplicationConverter.java
+++ backend/src/main/java/com/summerHack/diningTogether/converter/ApplicationConverter.java
@@ -1,4 +1,4 @@
-package com.summerHack.diningTogether.Converter;
+package com.summerHack.diningTogether.converter;
 
 import com.summerHack.diningTogether.DTO.ApplicationDTO;
 import com.summerHack.diningTogether.model.Application;
