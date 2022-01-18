cbe4f588d4403d5086330b4a7a629ae42fe87964
ZIQI TONG <ziqitong@ZIQIs-MacBook-Pro.local>
2022-01-18 11:11:44 +0800

    add home/about

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import AboutUs from "./pages/aboutUs/aboutUs.js"
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <AboutUs/> */}
+      <Link to="/aboutUs">About Us</Link> |{" "}
+      <Link to="/home"> Home</Link> |{" "}
+      <Link to="/test"> test</Link> |{" "}
+
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import AboutUs from './pages/aboutUs/aboutUs';
+import { render } from "react-dom";
+import Homepage from './pages/Homepage/homepage';
+import BasicGrid from "./components/foodList"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+
+const rootElement = document.getElementById("root");
+  render(
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="aboutUs" element={<AboutUs />} />
+      <Route path="home" element={<Homepage />} />
+      <Route path="test" element={<BasicGrid />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+  );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
