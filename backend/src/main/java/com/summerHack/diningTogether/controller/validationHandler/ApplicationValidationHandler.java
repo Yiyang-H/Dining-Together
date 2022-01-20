d71991bd58ab278bc959b6e9723148dd3d86594b
Liu233w <gh@liu233w.com>
2022-01-20 11:48:46 +1100

    Remove validation handler

--- backend/src/main/java/com/summerHack/diningTogether/controller/validationHandler/ApplicationValidationHandler.java
+++
@@ -1,9 +0,0 @@
-package com.summerHack.diningTogether.controller.validationHandler;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-@ControllerAdvice
-public class ApplicationValidationHandler extends ResponseEntityExceptionHandler {
-
-}
--- backend/src/main/java/com/summerHack/diningTogether/controller/validationHandler/UserValidationHandler.java
+++
@@ -1,8 +0,0 @@
-package com.summerHack.diningTogether.controller.validationHandler;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-@ControllerAdvice
-public class UserValidationHandler extends ResponseEntityExceptionHandler {
-}
