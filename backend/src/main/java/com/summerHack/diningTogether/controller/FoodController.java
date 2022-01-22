acd4fe854d1057d5072e5f988467be03db3aaf25
yuntaol <yuntao.lu@student.unimelb.edu.au>
2022-01-22 12:42:09 +1100

    modify get confirmed food bug

--- backend/src/main/java/com/summerHack/diningTogether/controller/FoodController.java
+++ backend/src/main/java/com/summerHack/diningTogether/controller/FoodController.java
@@ -33,9 +33,9 @@ public class FoodController {
     @GetMapping("/")
     public List<FoodDTO> getAllFoods(
         @RequestParam Optional<Category> category,
-        @RequestParam Optional<Boolean> confirmed) {
+        @RequestParam Optional<Boolean> completed) {
 
-        return this.foodService.findAll(category, confirmed);
+        return this.foodService.findAll(category, completed);
     }
 
     @Operation(summary = "show food by id")
