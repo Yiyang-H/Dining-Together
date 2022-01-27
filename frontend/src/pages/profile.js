294157b296db5d7dc45c5f654b398ce843663c15
ziqitong <zuttertop404@gmail.com>
2022-01-27 18:57:14 +0800

    fix userdata

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -22,7 +22,7 @@ export default function Profile(){
 
   useEffect(()=>{
     if(isLoggedIn()){
-      userId=getUserId();
+      setUserId(getUserId());
     //   setDetails(fetchUserData());
     //   setOrderDetails(fetchUsersOrder())
     }
